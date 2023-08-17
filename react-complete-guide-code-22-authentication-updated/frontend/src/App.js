961ee0d154c994de37a760161848b386ba31d431
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2023-08-18 00:32:47 +0100

    All files for this section added

---
+++ react-complete-guide-code-22-authentication-updated/frontend/src/App.js
@@ -0,0 +1,86 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import React from "react";
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
+import AuthenticationPage, { AuthAction } from "./pages/Authentication";
+import { logoutAction } from "./pages/Logout";
+import { checkAuthLoader, tokenLoader } from "./utils/auth";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    loader: tokenLoader,
+    id: "root",
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+                loader: checkAuthLoader,
+              },
+            ],
+          },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+            loader: checkAuthLoader,
+          },
+        ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+      {
+        path: "auth",
+        element: <AuthenticationPage />,
+        action: AuthAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
---
+++ react-complete-guide-code-22-authentication-updated/frontend/src/pages/Logout.js
@@ -0,0 +1,8 @@
+import { redirect } from "react-router-dom";
+
+export function logoutAction() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("expiration");
+  return redirect("/");
+}
+//logoutAction
