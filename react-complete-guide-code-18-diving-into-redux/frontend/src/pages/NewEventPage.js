c33a4bdc3dd1489dd595293a4bd143daaa1eb487
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2023-08-18 01:22:30 +0100

    Most files added

--- react-complete-guide-code-18-diving-into-redux/frontend/src/pages/NewEventPage.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EventForm from "../components/EventForm";
-const NewEventPage = () => {
-  return <EventForm method={"post"} />;
-};
-
-export default NewEventPage;
-
