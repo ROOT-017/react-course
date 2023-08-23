2eeb96c75d6877cce9cd0914ba59b4b10b7e3086
NKWETACHA TERENCE <nkwetachaterence17@gmail.com>
2023-08-23 03:03:03 +0100

    Stating files

--- react-complete-guide-code-26-testing/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-import "./index.css";
-import App from "./App";
-
-/*ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot*/
-//Fix the issue above
-createRoot(document.getElementById("root")).render(<App />);
